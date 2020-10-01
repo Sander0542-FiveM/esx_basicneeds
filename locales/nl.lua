8b7196612e83034cf94ab2a6dcefd376020a9ee1
Job <LordChunk@users.noreply.github.com>
2020-10-01 16:23:03 +0000

    Updated bread eating translation

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,4 +1,4 @@
 Locales['nl'] = {
-  ['used_bread'] = 'je hebt een ~y~1x~s~ ~b~brood~s~ opgegeten',
+  ['used_bread'] = 'je hebt een ~y~1x~s~ ~b~brood~s~ gegeten',
   ['used_water'] = 'je hebt ~y~1x~s~ ~b~water~s~ gedronken',
 }
